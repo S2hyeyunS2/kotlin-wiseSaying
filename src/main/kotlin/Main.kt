d7fee10992ef3a9c94dc6a87dd19a75d68c7592c
S2hyeyunS2 <hhyeyun22@gamil.com>
2025-02-17 17:34:16 +0900

    2단계 완료

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,8 +8,13 @@ fun main() {
         print("명언) ")
         val input= readlnOrNull()!!.trim() //null을 return 할 일 없으니까 nullable 제외
 
-        if(input=="종료"){
+        if(input=="종료")
             break
+        else if (input=="등록"){
+            print("명언 : ")
+            val content= readlnOrNull()!!.trim()
+            print("작가 : ")
+            val author= readlnOrNull()!!.trim()
         }
     }
 }
