beb48870717fa002298bec658f0af50d0617ff0b
S2hyeyunS2 <hhyeyun22@gamil.com>
2025-02-17 17:28:03 +0900

    1단계 완료

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,15 @@
 package com.ll
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("== 명언 앱 ==")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    while(true) {
+        print("명언) ")
+        val input= readlnOrNull()!!.trim() //null을 return 할 일 없으니까 nullable 제외
+
+        if(input=="종료"){
+            break
+        }
     }
 }
