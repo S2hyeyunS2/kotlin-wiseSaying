dc70de9155d7f5e0046ee42f041ad9dfd0c1ed14
S2hyeyunS2 <hhyeyun22@gamil.com>
2025-02-18 12:28:32 +0900

    service 생성

--- src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/entity/WiseSaying.kt
+++ src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/entity/WiseSaying.kt
@@ -3,7 +3,7 @@ package com.ll.domain.wiseSaying.wiseSaying.entity
 data class WiseSaying(val id:Int,
                  var content:String,
                  var author:String) {
-    fun update(content: String, author: String) {
+    fun modify(content: String, author: String) {
         this.content = content
         this.author = author
     }
