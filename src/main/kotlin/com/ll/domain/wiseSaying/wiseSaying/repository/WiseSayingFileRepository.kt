50b185575d346b2d3ffb4d7712977965e35e37c6
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-02-18 16:37:24 +0900

    WiseSayingFileRepository save 구현

---
+++ src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingFileRepository.kt
@@ -0,0 +1,68 @@
+package com.ll.domain.wiseSaying.wiseSaying.repository
+
+import com.ll.domain.wiseSaying.wiseSaying.entity.WiseSaying
+import com.ll.global.app.AppConfig
+import java.nio.file.Path
+
+class WiseSayingFileRepository : WiseSayingRepository {
+    private var lastId = 0
+    private val wiseSayings = mutableListOf<WiseSaying>()
+
+    val tableDirPath: Path
+        get() {
+            return AppConfig.dbDirPath.resolve("wiseSaying")
+        }
+
+    override fun save(wiseSaying: WiseSaying): WiseSaying {
+        if (wiseSaying.isNew()) {
+            wiseSaying.id = ++lastId
+            wiseSayings.add(wiseSaying)
+        }
+
+        saveOnDisk(wiseSaying)
+
+        return wiseSaying
+    }
+
+    override fun isEmpty(): Boolean {
+        return wiseSayings.isEmpty()
+    }
+
+    override fun findAll(): List<WiseSaying> {
+        return wiseSayings
+    }
+
+    override fun findById(id: Int): WiseSaying? {
+        return tableDirPath
+            .toFile()
+            .listFiles()
+            ?.find { it.name == "${id}.json" }
+            ?.let { WiseSaying.fromJsonStr(it.readText()) }
+    }
+
+    override fun delete(wiseSaying: WiseSaying) {
+        wiseSayings.remove(wiseSaying)
+    }
+
+    override fun clear() {
+        lastId = 0
+        wiseSayings.clear()
+
+        tableDirPath.toFile().deleteRecursively()
+    }
+
+    private fun saveOnDisk(wiseSaying: WiseSaying) {
+        mkTableDirsIfNotExists()
+
+        val wiseSayingFile = tableDirPath.resolve("${wiseSaying.id}.json")
+        wiseSayingFile.toFile().writeText(wiseSaying.jsonStr)
+    }
+
+    private fun mkTableDirsIfNotExists() {
+        tableDirPath.toFile().run {
+            if (!exists()) {
+                mkdirs()
+            }
+        }
+    }
+}
--- src/main/kotlin/com/ll/global/bean/SingletonScope.kt
+++ src/main/kotlin/com/ll/global/bean/SingletonScope.kt
@@ -2,13 +2,15 @@ package com.ll.global.bean
 
 import com.ll.domain.system.system.controller.SystemController
 import com.ll.domain.wiseSaying.wiseSaying.controller.WiseSayingController
+import com.ll.domain.wiseSaying.wiseSaying.repository.WiseSayingFileRepository
 import com.ll.domain.wiseSaying.wiseSaying.repository.WiseSayingMemoryRepository
 import com.ll.domain.wiseSaying.wiseSaying.repository.WiseSayingRepository
 import com.ll.domain.wiseSaying.wiseSaying.service.WiseSayingService
 
 object SingletonScope {
-    val wiseSayingController by lazy {WiseSayingController()}
-    val wiseSayingService by lazy {WiseSayingService()}
+    val wiseSayingController by lazy { WiseSayingController() }
+    val wiseSayingService by lazy { WiseSayingService() }
     val wiseSayingRepository: WiseSayingRepository by lazy { WiseSayingMemoryRepository() }
+    val wiseSayingFileRepository by lazy { WiseSayingFileRepository() }
     val systemController by lazy { SystemController() }
 }
