c98b025619e79badb7d225da672e071d17fae68b
S2hyeyunS2 <hhyeyun22@gamil.com>
2025-02-18 12:08:32 +0900

    8단계 완료

--- src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/entity/WiseSaying.kt
+++ src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/entity/WiseSaying.kt
@@ -2,4 +2,9 @@ package com.ll.domain.wiseSaying.wiseSaying.entity
 
 data class WiseSaying(val id:Int,
                  var content:String,
-                 var author:String)
+                 var author:String) {
+    fun update(content: String, author: String) {
+        this.content = content
+        this.author = author
+    }
+}
