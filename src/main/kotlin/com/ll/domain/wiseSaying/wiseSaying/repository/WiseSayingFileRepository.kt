e9b41e0bad824986a44917a807796ea456c801b3
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-02-20 10:50:56 +0900

    saveLastId, loadLastId test 구현

--- src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingFileRepository.kt
+++ src/main/kotlin/com/ll/domain/wiseSaying/wiseSaying/repository/WiseSayingFileRepository.kt
@@ -5,9 +5,6 @@ import com.ll.global.app.AppConfig
 import java.nio.file.Path
 
 class WiseSayingFileRepository : WiseSayingRepository {
-    private var lastId = 0
-    private val wiseSayings = mutableListOf<WiseSaying>()
-
     val tableDirPath: Path
         get() {
             return AppConfig.dbDirPath.resolve("wiseSaying")
@@ -15,8 +12,7 @@ class WiseSayingFileRepository : WiseSayingRepository {
 
     override fun save(wiseSaying: WiseSaying): WiseSaying {
         if (wiseSaying.isNew()) {
-            wiseSaying.id = ++lastId
-            wiseSayings.add(wiseSaying)
+            wiseSaying.id = loadLastIdAndIncrease()
         }
 
         saveOnDisk(wiseSaying)
@@ -25,11 +21,11 @@ class WiseSayingFileRepository : WiseSayingRepository {
     }
 
     override fun isEmpty(): Boolean {
-        return wiseSayings.isEmpty()
+        return true
     }
 
     override fun findAll(): List<WiseSaying> {
-        return wiseSayings
+        return listOf()
     }
 
     override fun findById(id: Int): WiseSaying? {
@@ -41,13 +37,10 @@ class WiseSayingFileRepository : WiseSayingRepository {
     }
 
     override fun delete(wiseSaying: WiseSaying) {
-        wiseSayings.remove(wiseSaying)
+
     }
 
     override fun clear() {
-        lastId = 0
-        wiseSayings.clear()
-
         tableDirPath.toFile().deleteRecursively()
     }
 
@@ -64,5 +57,29 @@ class WiseSayingFileRepository : WiseSayingRepository {
                 mkdirs()
             }
         }
+    }
+
+    internal fun saveLastId(lastId: Int) {
+        mkTableDirsIfNotExists()
+        tableDirPath.resolve("lastId.txt")
+            .toFile()
+            .writeText(lastId.toString())
+    }
+
+    internal fun loadLastId(): Int {
+        return try{
+            tableDirPath.resolve("lastId.txt")
+                .toFile()
+                .readText()
+                .toInt()
+        }catch (e: Exception){
+            0
+        }
+    }
+
+    private fun loadLastIdAndIncrease(): Int{
+        val lastId=loadLastId()
+        saveLastId(lastId+1)
+        return lastId
     }
 }
